5758e2e122c34b92a6e52305dadc75a9b12c55e1
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2019-12-21 22:06:42 +0100

    Update cross_validation.py

--- cross_validation.py
+++ cross_validation.py
@@ -2,3 +2,51 @@ from sklearn.model_selection import cross_val_score
 scores = cross_val_score(tree_reg, housing_prepared, housing_labels,
 scoring="neg_mean_squared_error", cv=10)
 rmse_scores = np.sqrt(-scores)
+
+
+#### Grid search CV
+from sklearn.model_selection import GridSearchCV
+param_grid = [
+{'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+{'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+]
+forest_reg = RandomForestRegressor()
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+scoring='neg_mean_squared_error')
+grid_search.fit(housing_prepared, housing_labels)
+
+>>> grid_search.best_params_
+{'max_features': 6, 'n_estimators': 30}
+
+>>> grid_search.best_estimator_
+RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=None,
+max_features=6, max_leaf_nodes=None, min_samples_leaf=1,
+min_samples_split=2, min_weight_fraction_leaf=0.0,
+n_estimators=30, n_jobs=1, oob_score=False, random_state=None,
+verbose=0, warm_start=False)
+
+>>> cvres = grid_search.cv_results_
+... for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+...
+print(np.sqrt(-mean_score), params)
+...
+64912.0351358 {'max_features': 2, 'n_estimators': 3}
+55535.2786524 {'max_features': 2, 'n_estimators': 10}
+52940.2696165 {'max_features': 2, 'n_estimators': 30}
+60384.0908354 {'max_features': 4, 'n_estimators': 3}
+52709.9199934 {'max_features': 4, 'n_estimators': 10}
+50503.5985321 {'max_features': 4, 'n_estimators': 30}
+59058.1153485 {'max_features': 6, 'n_estimators': 3}
+52172.0292957 {'max_features': 6, 'n_estimators': 10}
+49958.9555932 {'max_features': 6, 'n_estimators': 30}
+59122.260006 {'max_features': 8, 'n_estimators': 3}
+52441.5896087 {'max_features': 8, 'n_estimators': 10}
+50041.4899416 {'max_features': 8, 'n_estimators': 30}
+62371.1221202 {'bootstrap': False, 'max_features': 2, 'n_estimators': 3}
+54572.2557534 {'bootstrap': False, 'max_features': 2, 'n_estimators': 10}
+59634.0533132 {'bootstrap': False, 'max_features': 3, 'n_estimators': 3}
+52456.0883904 {'bootstrap': False, 'max_features': 3, 'n_estimators': 10}
+58825.665239 {'bootstrap': False, 'max_features': 4, 'n_estimators': 3}
+52012.9945396 {'bootstrap': False, 'max_features': 4, 'n_estimators': 10}
+
+### Think about RandomizeSearch
